ac64b0830b5d017f24d1594adf0ef5706a6ddf79
aryalg1@nku.edu <aryalg1@nku.edu>
2016-09-17 13:06:11 -0400

    Exercise from Chapter 22.

---
+++ app/src/main/java/android/nku/edu/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,17 @@
+package android.nku.edu.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+
+
+
+
+}
